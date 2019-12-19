3fb60c2ead2a9f4724ae1d592f31437f4baf39f5
Jannis <jannis@pixel-pink.de>
2019-12-19 17:09:49 +0100

    template component

--- src/components/AANewComponent.js
+++ src/components/AANewComponent.js
@@ -1,22 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import styled from "styled-components"
-import logo from "../../content/logos/logo.svg"
-import Icon from "./layout/Icon"
-import { ICONS } from "../theme/Icons"
+// import logo from "../../content/logos/logo.svg"
+// import Icon from "./layout/Icon"
+// import { ICONS } from "../theme/Icons"
 
 const StyledNav = styled.div`
-
     @media (min-width: 834px) {
         display: none;
     }
 `
 
-
-const Nav = () => (
-    <StyledNav>
-       
-    </StyledNav>
-)
+const Nav = () => <StyledNav></StyledNav>
 
 export default Nav
