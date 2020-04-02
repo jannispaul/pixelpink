03b934bfc0d6e992b5dd22eb44a89cc4356b85ff
Jannis <jannis@pixel-pink.de>
2020-04-02 14:47:02 +0200

    image margins

--- src/components/OurProcessImage.js
+++ src/components/OurProcessImage.js
@@ -4,17 +4,19 @@ import Img from "gatsby-image"
 import styled from "styled-components"
 
 const StyledIMG = styled(Img)`
-    margin-top: 150px;
+    margin-top: 50px;
     @media (min-width: 769px) {
+        margin-top: 100px;
     }
     @media (min-width: 1230px) {
+        margin-top: 150px;
     }
     @media (min-width: 1441px) {
     }
 `
 
 const Image = () => (
-    <StaticQuery 
+    <StaticQuery
         query={graphql`
             query {
                 serviceimage: file(relativePath: { eq: "our-process.png" }) {
