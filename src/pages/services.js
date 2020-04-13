a90812057624562e0df5232a378c6a4fa3a7e6e1
Jannis <jannis@pixel-pink.de>
2020-04-13 15:59:46 +0200

    ttext fix

--- src/pages/services.js
+++ src/pages/services.js
@@ -26,7 +26,7 @@ class ServicePage extends React.Component {
                         `Was tun wir? In einem Satz: Wir helfen Dir, eine passende Digital-Strategie zu finden! Unsere interdisziplinären Kompetenzen ermöglichen es uns, für unsere Kunden besondere Konzepte zu kreieren, von denen ihre Nutzer begeistert sind.`,
                     ]}
                     body2={[
-                        `Wir verfolgen einen emphatischen Design-Ansatz. So, dass wir immer die Sicht Deiner Nutzer im Blick haben und unsere diesbezüglichen Erkenntnisse in allen Phasen Deines Projektes einfließen lassen. Unsere Services Konzeption, Design, Entwicklung und Marketing sind so miteinander verbunden und profitieren durch den Austausch voneinander.`,
+                        `Wir verfolgen einen emphatischen Design-Ansatz. Damit haben wir immer die Sicht Deiner Nutzer im Blick und lassen unsere diesbezüglichen Erkenntnisse in allen Phasen Deines Projektes einfließen. Unsere Services Konzeption, Design, Entwicklung und Marketing sind so miteinander verbunden und profitieren durch den Austausch voneinander.`,
                     ]}
                 />
                 <FiftyFifty />
