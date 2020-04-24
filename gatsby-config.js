04f644df6b85d253490422cdf97dd7315568bf80
Jannis <jannis@pixel-pink.de>
2020-04-24 14:33:48 +0200

    added netlify redirect for language

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,11 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `pixelpink`,
+                short_name: `pp`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#fff`,
+                theme_color: `#fff`,
                 display: `minimal-ui`,
                 icon: `content/images/favicon.png`, // This path is relative to the root of the site.
             },
